284933510c4f3cf9ef90a6ff8823b620fc2f396e
Miler-arch <miller4290@hotmail.com>
2023-11-16 16:29:20 -0400

    add updates report versions

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use App\Models\Course;
 use App\Models\DetailRegister;
 use Illuminate\Http\Request;
@@ -14,17 +15,21 @@ class ReportController extends Controller
         $courses = Course::where('status', '1')->get();
         return view('reports.index', compact('courses', 'reports'));
     }
-public function getVersion($course_id)
-{
-    $detailRegisters = DetailRegister::whereHas('course', function ($query) use ($course_id) {
-        $query->where('id', $course_id);
-    })->get();
-    $clients = $detailRegisters->pluck('client')->unique();
-    dd($clients);
+    public function getVersion(Request $request)
+    {
+        $course_id = $request->input('course_id');
 
-    $pdf = \PDF::loadView('reports.version', compact('clients', 'detailRegisters'));
-    return $pdf->stream('version.pdf');
-}
+        $detailRegisters = DetailRegister::whereHas('course', function ($query) use ($course_id) {
+            $query->where('id', $course_id);
+        })->get();
 
+        $clientIds = $detailRegisters->pluck('client_id')->unique();
+        $clients = Client::whereIn('id', $clientIds)->get();
 
+        // Obtener información del curso seleccionado
+        $selectedCourse = Course::find($course_id);
+    
+        $pdf = \PDF::loadView('reports.version', compact('clients', 'detailRegisters', 'selectedCourse'));
+        return $pdf->stream('version.pdf');
+    }
 }
